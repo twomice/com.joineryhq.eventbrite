99fd866046bd550bcdac6fc63a187fb00a736a59
Allen Shaw <allen@JoineryHQ.com>
2019-06-03 15:14:12 -0500

    Toward queue processing.

---
+++ CRM/Eventbrite/Eventbrite.mgd.php
@@ -0,0 +1,23 @@
+<?php
+// This file declares managed database records.
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see 'hook_civicrm_managed' at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  'ParticipantStatus_Removed' => array (
+    'name' => 'ParticipantStatus_Removed',
+    'entity' => 'ParticipantStatusType',
+    'params' =>
+    array (
+      'version' => 3,
+      'name' => 'Removed_in_EventBrite',
+      'label' => 'Removed in EventBrite',
+      'class' => 'Negative',
+      'is_reserved' => '1',
+      'is_active' => '1',
+      'is_counted' => '0',
+      'visibility_id' => '2',
+      'weight' => '1000',
+    ),
+  ),
+);
---
+++ api/v3/Eventbrite/Runqueue.mgd.php
@@ -0,0 +1,22 @@
+<?php
+// This file declares a managed database record of type "Job".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'Cron:Eventbrite.Runqueue',
+    'entity' => 'Job',
+    'params' => 
+    array (
+      'version' => 3,
+      'name' => 'Call Eventbrite.Runqueue API',
+      'description' => 'Call Eventbrite.Runqueue API',
+      'run_frequency' => 'Always',
+      'api_entity' => 'Eventbrite',
+      'api_action' => 'Runqueue',
+      'parameters' => 'runInNonProductionEnvironment=TRUE',
+    ),
+  ),
+);
