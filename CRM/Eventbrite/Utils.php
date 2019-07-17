a76676824a6fa7535bac66cda281dc853ebef922
Pengyi Zhang <pengyi@agileware.com.au>
2019-07-17 17:11:29 +1000

    CIVICRM-1278 fix the webhook url

--- CRM/Eventbrite/Utils.php
+++ CRM/Eventbrite/Utils.php
@@ -7,7 +7,19 @@
  */
 class CRM_Eventbrite_Utils {
   static public function getWebhookListenerUrl() {
-    return CRM_Utils_System::url('civicrm/eventbrite/webhook', NULL, TRUE);
+    $url = CRM_Utils_System::url('civicrm/eventbrite/webhook',
+      NULL,
+      TRUE,
+      NULL,
+      FALSE,
+      FALSE,
+      FALSE);
+    if (strpos($url, 'wp-admin/admin.php') !== FALSE) {
+      Civi::log()->info("Eventbrite: fix URL for WordPress");
+      $url = str_replace('/wp-admin/admin.php', '', $url);
+    }
+    Civi::log()->info("Eventbrite webhook url: " . $url);
+    return $url;
   }
 
 }
